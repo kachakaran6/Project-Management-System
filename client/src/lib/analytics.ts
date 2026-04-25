2ffe9ec86d0e243f5ffab9acee0fb06a52d9c6fe
kachakaran6 <kachakaran06@gmail.com>
2026-04-26 02:55:09 +0530

    google analytics added

---
+++ client/src/lib/analytics.ts
@@ -0,0 +1,20 @@
+// analytics.ts
+export const GA_TRACKING_ID = 'G-N39B1ZES4S';
+
+export const trackPageView = (url: string) => {
+  if (typeof window !== 'undefined' && (window as any).gtag) {
+    (window as any).gtag('config', GA_TRACKING_ID, {
+      page_path: url,
+    });
+  }
+};
+
+export const trackEvent = (action: string, category: string, label: string, value?: number) => {
+  if (typeof window !== 'undefined' && (window as any).gtag) {
+    (window as any).gtag('event', action, {
+      event_category: category,
+      event_label: label,
+      value: value,
+    });
+  }
+};
